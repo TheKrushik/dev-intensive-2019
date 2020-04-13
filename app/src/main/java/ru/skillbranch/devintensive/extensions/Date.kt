09aaae497502130acc65b453508dda26063a233b
a.krushinskiy <a.krushinskiy@pokerdom.com>
2020-04-13 15:20:01 +0300

    Date.humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,6 +1,5 @@
 package ru.skillbranch.devintensive.extensions
 
-import java.lang.IllegalStateException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -28,10 +27,38 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    TODO("Not yet implemented")
+    val diff = date.time - this.time
+    return when (diff) {
+        in 0 until SECOND -> "только что"
+        in SECOND until 45 * SECOND -> "несколько секунд назад"
+        in 45 * SECOND until 75 * SECOND -> "минуту назад"
+        in 75 * SECOND until 300 * SECOND -> "${(diff / MINUTE)} минуты назад"
+        in 300 * SECOND until 45 * MINUTE -> "${(diff / MINUTE)} минут назад"
+        in 5 * MINUTE until 75 * MINUTE -> "час назад"
+        in 75 * MINUTE until 300 * MINUTE -> "${(diff / HOUR)} часа назад"
+        in 300 * MINUTE until 22 * HOUR -> "${(diff / HOUR)} часов назад"
+        in 22 * HOUR until 26 * HOUR -> "день назад"
+        in 26 * HOUR until 100 * HOUR -> "${(diff / DAY)} дня назад"
+        in 100 * HOUR until 360 * DAY -> "${(diff / DAY)} дней назад"
+        in 360 * DAY until Long.MAX_VALUE -> "более года назад"
+
+        in -(SECOND) until 0 -> "сейчас"
+        in -(45 * SECOND) until -(SECOND) -> "через несколько секунд"
+        in -(75 * SECOND) until -(45 * SECOND) -> "через минуту"
+        in -(300 * SECOND) until -(75 * SECOND) -> "через ${(diff / MINUTE) * (-1)} минуты"
+        in -(45 * MINUTE) until -(300 * SECOND) -> "через ${(diff / MINUTE) * (-1)} минут"
+        in -(75 * MINUTE) until -(45 * MINUTE) -> "через час"
+        in -(300 * MINUTE) until -(75 * MINUTE) -> "через ${(diff / HOUR) * (-1)} часа"
+        in -(22 * HOUR) until -(300 * MINUTE) -> "через ${(diff / HOUR) * (-1)} часов"
+        in -(26 * HOUR) until -(22 * HOUR) -> "через день"
+        in -(100 * HOUR) until -(26 * HOUR) -> "через ${(diff / DAY) * (-1)} дня"
+        in -(360 * DAY) until -(100 * HOUR) -> "через ${(diff / DAY) * (-1)} дней"
+        in -(360 * DAY) until Long.MIN_VALUE -> "более чем через год"
+        else -> throw IllegalStateException("invalid data")
+    }
 }
 
-enum class TimeUnits{
+enum class TimeUnits {
     SECOND,
     MINUTE,
     HOUR,
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -20,57 +20,55 @@ object Utils {
         for (c in payload) {
             var char = c
             val isTitle = char.isUpperCase()
-            if (isTitle) {
-                val temp = char.toLowerCase()
-                char = temp
-            }
+            if (isTitle) char = char.toLowerCase()
 
-            var word = when (char) {
-                ' ' -> ""
-                'а' -> "a"
-                'б' -> "b"
-                'в' -> "v"
-                'г' -> "g"
-                'д' -> "d"
-                'е' -> "e"
-                'ё' -> "jo"
-                'ж' -> "zh"
-                'з' -> "z"
-                'и' -> "i"
-                'й' -> "j"
-                'к' -> "k"
-                'л' -> "l"
-                'м' -> "m"
-                'н' -> "n"
-                'о' -> "o"
-                'п' -> "p"
-                'р' -> "r"
-                'с' -> "s"
-                'т' -> "t"
-                'у' -> "u"
-                'ф' -> "f"
-                'х' -> "h"
-                'ц' -> "c"
-                'ч' -> "ch"
-                'ш' -> "sh"
-                'щ' -> "shh"
-                'ъ' -> ""
-                'ы' -> "y"
-                'ь' -> "'"
-                'э' -> "je"
-                'ю' -> "yu"
-                'я' -> "ya"
-                else -> char.toString()
-            }
+            var word = setCharToString(char)
 
-            if (isTitle) {
-                val temp = word.capitalize()
-                word = temp
-            }
+            if (isTitle) word = word.capitalize()
 
             result += word
         }
         return result
+    }
+
+    private fun setCharToString(char: Char): String {
+        return when (char) {
+            ' ' -> ""
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е' -> "e"
+            'ё' -> "jo"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и' -> "i"
+            'й' -> "j"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "shh"
+            'ъ' -> ""
+            'ы' -> "y"
+            'ь' -> "'"
+            'э' -> "je"
+            'ю' -> "yu"
+            'я' -> "ya"
+            else -> char.toString()
+        }
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
